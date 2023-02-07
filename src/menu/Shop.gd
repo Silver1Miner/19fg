2110361c584451b008402066cf2161ad6bd95b96
Your Name <silver1miner@gmail.com>
2023-02-06 19:01:31 -0800

    store ui

--- src/menu/Shop.gd
+++ src/menu/Shop.gd
@@ -1,9 +1,36 @@
 extends Control
 
 signal back()
+onready var anim = $AnimationPlayer
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _on_Back_button_up() -> void:
 	emit_signal("back")
+
+func _on_BuyTip_button_up() -> void:
+	attempt_cash_purchase(0)
+
+func _on_BuyGems1_button_up() -> void:
+	attempt_cash_purchase(1)
+
+func _on_BuyGems2_button_up() -> void:
+	attempt_cash_purchase(2)
+
+func _on_BuyGems3_button_up() -> void:
+	attempt_cash_purchase(3)
+
+func attempt_cash_purchase(index: int) -> void:
+	if anim.is_playing():
+		return
+	if Billing.android_iap:
+		print("attempting purchase via android")
+		Billing.android_purchase(index)
+	elif Billing.ios_iap:
+		print("attempting purchase via iOS")
+		Billing.ios_purchase(index)
+	else:
+		print("no shop connected")
+		if not OS.get_name() in ["Android", "iOS"]:
+			Billing.non_mobile_testing(index)
